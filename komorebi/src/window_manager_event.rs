13e2cbc7a1a4bb6f0c29227ec0461ae6e83f96d2
LGUG2Z <LGUG2Z@fastmail.com>
2024-08-06 16:41:50 -0700

    fix(wm): exclude minimized hwnds from show event
    This commit ensures that a WindowManagerEvent::Show will not be
    triggered when a WinEvent::ObjectNameChange is received for an
    application in the object_name_change_on_launch whitelist.
    
    This notably impacts Firefox when the window title changes while the
    application is minimized (for example, on a page with YouTube autoplay
    enabled).
    
    fix #941

--- komorebi/src/window_manager_event.rs
+++ komorebi/src/window_manager_event.rs
@@ -151,7 +151,10 @@ impl WindowManagerEvent {
                 )
                 .is_some();
 
-                if should_trigger_show {
+                // should not trigger show on minimized windows, for example when firefox sends
+                // this message due to youtube autoplay changing the window title
+                // https://github.com/LGUG2Z/komorebi/issues/941
+                if should_trigger_show && !window.is_miminized() {
                     Option::from(Self::Show(winevent, window))
                 } else {
                     Option::from(Self::TitleUpdate(winevent, window))
