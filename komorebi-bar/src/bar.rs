665a45afedaa6ba15e9d7adfa4b705753c7e40cc
LGUG2Z <LGUG2Z@fastmail.com>
2024-10-06 13:02:04 -0700

    refactor(bar): use native apis for positioning
    This commit replaces almost all uses of the egui API for bar window
    positioning with calls to SetWindowPos via komorebi_client's Window
    struct.
    
    This seems to play much more smoothly with multi-monitor setups where
    each monitor has a different scaling factor, opening the door for
    multiple instances of komorebi-bar.exe to run against multiple monitors.
    
    As a result of this change, the "viewport" configuration option has been
    renamed to "position" and doc strings have been changed to remove the
    reference to the egui crate docs. Similarly, "viewport.position" and
    "viewport.inner_size" have been renamed to "position.start" and
    "position.end" respectively. Backwards-compatibility aliases have been
    included for all renames.

--- komorebi-bar/src/bar.rs
+++ komorebi-bar/src/bar.rs
@@ -1,11 +1,17 @@
 use crate::config::KomobarConfig;
 use crate::config::KomobarTheme;
+use crate::config::Position;
+use crate::config::PositionConfig;
 use crate::komorebi::Komorebi;
 use crate::komorebi::KomorebiNotificationState;
+use crate::process_hwnd;
 use crate::widget::BarWidget;
 use crate::widget::WidgetConfig;
-use crate::DPI;
+use crate::BAR_HEIGHT;
 use crate::MAX_LABEL_WIDTH;
+use crate::MONITOR_LEFT;
+use crate::MONITOR_RIGHT;
+use crate::MONITOR_TOP;
 use crossbeam_channel::Receiver;
 use eframe::egui::Align;
 use eframe::egui::CentralPanel;
@@ -18,11 +24,8 @@ use eframe::egui::FontId;
 use eframe::egui::Frame;
 use eframe::egui::Layout;
 use eframe::egui::Margin;
-use eframe::egui::Pos2;
 use eframe::egui::Style;
 use eframe::egui::TextStyle;
-use eframe::egui::Vec2;
-use eframe::egui::ViewportCommand;
 use font_loader::system_fonts;
 use font_loader::system_fonts::FontPropertyBuilder;
 use komorebi_client::KomorebiTheme;
@@ -146,16 +149,43 @@ impl Komobar {
             Self::add_custom_font(ctx, font_family);
         }
 
-        if let Some(viewport) = &config.viewport {
-            let dpi = DPI.load(Ordering::SeqCst);
-            if let Some(position) = viewport.position {
-                let pos2 = Pos2::new(position.x / dpi, position.y / dpi);
-                ctx.send_viewport_cmd(ViewportCommand::OuterPosition(pos2));
-            }
+        let position = config.position.clone().unwrap_or(PositionConfig {
+            start: Some(Position {
+                x: MONITOR_LEFT.load(Ordering::SeqCst) as f32,
+                y: MONITOR_TOP.load(Ordering::SeqCst) as f32,
+            }),
+            end: Some(Position {
+                x: MONITOR_RIGHT.load(Ordering::SeqCst) as f32,
+                y: BAR_HEIGHT,
+            }),
+        });
 
-            if let Some(position) = viewport.inner_size {
-                let vec2 = Vec2::new(position.x / dpi, position.y / dpi);
-                ctx.send_viewport_cmd(ViewportCommand::InnerSize(vec2));
+        if let Some(hwnd) = process_hwnd() {
+            let start = position.start.unwrap_or(Position {
+                x: MONITOR_LEFT.load(Ordering::SeqCst) as f32,
+                y: MONITOR_TOP.load(Ordering::SeqCst) as f32,
+            });
+
+            let end = position.end.unwrap_or(Position {
+                x: MONITOR_RIGHT.load(Ordering::SeqCst) as f32,
+                y: BAR_HEIGHT,
+            });
+
+            let rect = komorebi_client::Rect {
+                left: start.x as i32,
+                top: start.y as i32,
+                right: end.x as i32,
+                bottom: end.y as i32,
+            };
+
+            let window = komorebi_client::Window::from(hwnd);
+            match window.set_position(&rect, false) {
+                Ok(_) => {
+                    tracing::info!("updated bar position");
+                }
+                Err(error) => {
+                    tracing::error!("{}", error.to_string())
+                }
             }
         }
 
@@ -294,6 +324,8 @@ impl Komobar {
             scale_factor: cc.egui_ctx.native_pixels_per_point().unwrap_or(1.0),
         };
 
+        komobar.apply_config(&cc.egui_ctx, &config, None);
+        // needs a double apply the first time for some reason
         komobar.apply_config(&cc.egui_ctx, &config, None);
 
         komobar
--- komorebi-bar/src/config.rs
+++ komorebi-bar/src/config.rs
@@ -7,13 +7,15 @@ use komorebi_client::Rect;
 use schemars::JsonSchema;
 use serde::Deserialize;
 use serde::Serialize;
+use std::collections::HashMap;
 use std::path::PathBuf;
 
 #[derive(Clone, Debug, Serialize, Deserialize, JsonSchema)]
 /// The `komorebi.bar.json` configuration file reference for `v0.1.30`
 pub struct KomobarConfig {
-    /// Viewport options (see: https://docs.rs/egui/latest/egui/viewport/struct.ViewportBuilder.html)
-    pub viewport: Option<ViewportConfig>,
+    /// Bar positioning options
+    #[serde(alias = "viewport")]
+    pub position: Option<PositionConfig>,
     /// Frame options (see: https://docs.rs/egui/latest/egui/containers/struct.Frame.html)
     pub frame: Option<FrameConfig>,
     /// Monitor options
@@ -32,12 +34,43 @@ pub struct KomobarConfig {
     pub right_widgets: Vec<WidgetConfig>,
 }
 
+impl KomobarConfig {
+    pub fn aliases(raw: &str) {
+        let mut map = HashMap::new();
+        map.insert("position", ["viewport"]);
+        map.insert("end", ["inner_frame"]);
+
+        let mut display = false;
+
+        for aliases in map.values() {
+            for a in aliases {
+                if raw.contains(a) {
+                    display = true;
+                }
+            }
+        }
+
+        if display {
+            println!("\nYour bar configuration file contains some options that have been renamed or deprecated:\n");
+            for (canonical, aliases) in map {
+                for alias in aliases {
+                    if raw.contains(alias) {
+                        println!(r#""{alias}" is now "{canonical}""#);
+                    }
+                }
+            }
+        }
+    }
+}
+
 #[derive(Clone, Debug, Serialize, Deserialize, JsonSchema)]
-pub struct ViewportConfig {
+pub struct PositionConfig {
     /// The desired starting position of the bar (0,0 = top left of the screen)
-    pub position: Option<Position>,
+    #[serde(alias = "position")]
+    pub start: Option<Position>,
     /// The desired size of the bar from the starting position (usually monitor width x desired height)
-    pub inner_size: Option<Position>,
+    #[serde(alias = "inner_size")]
+    pub end: Option<Position>,
 }
 
 #[derive(Clone, Debug, Serialize, Deserialize, JsonSchema)]
