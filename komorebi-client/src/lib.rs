4d23156f2a196c0731d0091f92fc2ae71236dddd
alex-ds13 <145657253+alex-ds13@users.noreply.github.com>
2024-12-22 10:44:15 +0000

    fix: reexport a bunch of structs on komorebi-client

--- komorebi-client/src/lib.rs
+++ komorebi-client/src/lib.rs
@@ -2,6 +2,7 @@
 #![allow(clippy::missing_errors_doc)]
 
 pub use komorebi::animation::prefix::AnimationPrefix;
+pub use komorebi::animation::PerAnimationPrefixConfig;
 pub use komorebi::asc::ApplicationSpecificConfiguration;
 pub use komorebi::colour::Colour;
 pub use komorebi::colour::Rgb;
@@ -39,9 +40,12 @@ pub use komorebi::ring::Ring;
 pub use komorebi::window::Window;
 pub use komorebi::window_manager_event::WindowManagerEvent;
 pub use komorebi::workspace::Workspace;
+pub use komorebi::AnimationsConfig;
 pub use komorebi::BorderColours;
+pub use komorebi::CrossBoundaryBehaviour;
 pub use komorebi::GlobalState;
 pub use komorebi::KomorebiTheme;
+pub use komorebi::MonitorConfig;
 pub use komorebi::Notification;
 pub use komorebi::NotificationEvent;
 pub use komorebi::RuleDebug;
@@ -50,6 +54,8 @@ pub use komorebi::State;
 pub use komorebi::StaticConfig;
 pub use komorebi::SubscribeOptions;
 pub use komorebi::TabsConfig;
+pub use komorebi::WindowContainerBehaviour;
+pub use komorebi::WorkspaceConfig;
 
 use komorebi::DATA_DIR;
 
