5a8f48c6b9908a2b325891064116e2ba97c8e5f3
LGUG2Z <LGUG2Z@fastmail.com>
2025-01-03 18:20:02 -0800

    chore(dev): begin v0.1.33-dev

--- komorebi/src/static_config.rs
+++ komorebi/src/static_config.rs
@@ -238,7 +238,7 @@ impl From<&Monitor> for MonitorConfig {
 }
 
 #[derive(Debug, Serialize, Deserialize, JsonSchema)]
-/// The `komorebi.json` static configuration file reference for `v0.1.32`
+/// The `komorebi.json` static configuration file reference for `v0.1.33`
 pub struct StaticConfig {
     /// DEPRECATED from v0.1.22: no longer required
     #[serde(skip_serializing_if = "Option::is_none")]
