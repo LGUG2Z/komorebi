abf96ebbeb107575f7249c1e3772d616a1229d9e
Samu-K <samu.kaarlela@gmail.com>
2025-01-24 04:04:12 +0200

    Added ability for modifiers on strftime formatters

--- komorebi-bar/src/date.rs
+++ komorebi-bar/src/date.rs
@@ -13,14 +13,51 @@ use schemars::JsonSchema;
 use serde::Deserialize;
 use serde::Serialize;
 
+/// Allow modifiers for any integer formatter
+/// Use format: { CustomModifiers {"Format" : format_str, "Modifiers": { formatter: value } } }
+/// e.g { CustomModifiers {"Format" : "%D %U", "Modifiers": { "%U": 1 } } }
+/// To add one to week number
 #[derive(Clone, Debug, Serialize, Deserialize, JsonSchema)]
-#[serde(untagged)]
-pub enum Custom {
-    Simple(String),
-    WithModifiers {
-        Custom: String,
-        Modifiers: std::collections::HashMap<String, i32>,
-    },
+pub struct CustomModifiers {
+    Format: String,
+    Modifiers: std::collections::HashMap<String, i32>,
+}
+
+impl CustomModifiers {
+    fn apply(&self, output: &str) -> String {
+        let int_formatters = vec![
+            "%Y", "%C", "%y", "%m", "%d", "%e", "%w", "%u", "%U", "%W", "%G", "%g", "%V", "%j",
+            "%H", "%k", "%I", "%l", "%M", "%S", "%f",
+        ];
+
+        let mut modified_output = output.to_string();
+
+        for (modifier, value) in &self.Modifiers {
+<<<<<<< HEAD
+=======
+
+>>>>>>> 7e8af3d (Move modifiers date to own struct)
+            // check if formatter is integer type
+            if !int_formatters.contains(&modifier.as_str()) {
+                continue;
+            }
+
+            // get the strftime value of modifier
+            let formatted_modifier = chrono::Local::now().format(modifier).to_string();
+
+            // find the gotten value in the original output
+            if let Some(pos) = modified_output.find(&formatted_modifier) {
+                let start = pos;
+                let end = start + formatted_modifier.len();
+                // replace that value with the modified value
+                if let Ok(num) = formatted_modifier.parse::<i32>() {
+                    modified_output.replace_range(start..end, &(num + value).to_string());
+                }
+            }
+        }
+
+        modified_output
+    }
 }
 
 #[derive(Clone, Debug, Serialize, Deserialize, JsonSchema)]
@@ -54,9 +91,9 @@ pub enum DateFormat {
     /// Day Date Month Year format (8 September 2024)
     DayDateMonthYear,
     /// Custom format (https://docs.rs/chrono/latest/chrono/format/strftime/index.html)
-    /// Allow modifiers for any integer formatter
-    /// Use format: { Custom: "format_str", "Modifiers": { "formatter": value } }
-    Custom(Custom),
+    Custom(String),
+    /// Custom format with modifiers
+    CustomModifiers(CustomModifiers),
 }
 
 impl DateFormat {
@@ -76,69 +113,14 @@ impl DateFormat {
             DateFormat::YearMonthDate => String::from("%F"),
             DateFormat::DateMonthYear => String::from("%v"),
             DateFormat::DayDateMonthYear => String::from("%A %e %B %Y"),
-            DateFormat::Custom(Custom) => match Custom {
-                Custom::Simple(fmt) => fmt.clone(),
-                Custom::WithModifiers { Custom, .. } => Custom.clone(),
-            },
+            DateFormat::Custom(custom) => custom.to_string(),
+            DateFormat::CustomModifiers(custom) => custom.Format.clone(),
         }
     }
+<<<<<<< HEAD
+=======
 
-    fn apply_modifiers(&self, output: &str) -> String {
-        // contains all strftime formatters that return integers
-        let int_formatters = vec!(
-            "%Y",
-            "%C",
-            "%y%",
-            "%m",
-            "%d",
-            "%e",
-            "%w",
-            "%u",
-            "%U",
-            "%W",
-            "%G",
-            "%g",
-            "%V",
-            "%j",
-            "%H",
-            "%k",
-            "%I",
-            "%l",
-            "%M",
-            "%S",
-            "%f"
-        );
-
-        match self {
-            // unwrap the Custom enum
-            DateFormat::Custom(Custom::WithModifiers { Modifiers: modifiers, .. }) => {
-                let mut modified_output = output.to_string();
-
-                // iterate over the modifiers
-                for (modifier, value) in modifiers {
-                    // only run if int formatters are used
-                    if !int_formatters.contains(&modifier.as_str()) {
-                        continue;
-                    }
-
-                    // get the strftime value of modifier
-                    let formatted_modifier = chrono::Local::now().format(modifier).to_string();
-
-                    // find the original value in the original output
-                    if let Some(pos) = modified_output.find(&formatted_modifier) {
-                        let start = pos;
-                        let end = start + formatted_modifier.len();
-                        // replace the original value with the modified value
-                        if let Ok(num) = formatted_modifier.parse::<i32>() {
-                            modified_output.replace_range(start..end, &(num + value).to_string());
-                        }
-                    }
-                }
-                modified_output
-            }
-            _ => output.to_string(),
-        }
-    }
+>>>>>>> 7e8af3d (Move modifiers date to own struct)
 }
 
 #[derive(Clone, Debug)]
@@ -150,11 +132,13 @@ pub struct Date {
 
 impl Date {
     fn output(&mut self) -> String {
-        let formatted = chrono::Local::now().format(&self.format.fmt_string()).to_string();
+        let formatted = chrono::Local::now()
+            .format(&self.format.fmt_string())
+            .to_string();
 
-        // if modifiers are present, apply them
+        // if custom modifiers are used, apply them
         match &self.format {
-            DateFormat::Custom { .. } => self.format.apply_modifiers(&formatted),
+            DateFormat::CustomModifiers(custom) => custom.apply(&formatted),
             _ => formatted,
         }
     }
--- komorebi/src/window.rs
+++ komorebi/src/window.rs
@@ -15,6 +15,7 @@ use crate::focus_manager;
 use crate::stackbar_manager;
 use crate::windows_api;
 use crate::AnimationStyle;
+use crate::FLOATING_WINDOW_TOGGLE_ASPECT_RATIO;
 use crate::SLOW_APPLICATION_COMPENSATION_TIME;
 use crate::SLOW_APPLICATION_IDENTIFIERS;
 use std::collections::HashMap;
@@ -296,6 +297,30 @@ impl RenderDispatcher for TransparencyRenderDispatcher {
     }
 }
 
+#[derive(Copy, Clone, Debug, Serialize, Deserialize, JsonSchema)]
+#[serde(untagged)]
+pub enum AspectRatio {
+    /// 21:9
+    Ultrawide,
+    /// 16:9
+    Widescreen,
+    /// 4:3
+    Standard,
+    /// A custom W:H aspect ratio
+    Custom(i32, i32),
+}
+
+impl AspectRatio {
+    pub fn width_and_height(self) -> (i32, i32) {
+        match self {
+            AspectRatio::Ultrawide => (21, 9),
+            AspectRatio::Widescreen => (16, 9),
+            AspectRatio::Standard => (4, 3),
+            AspectRatio::Custom(w, h) => (w, h),
+        }
+    }
+}
+
 impl Window {
     pub const fn hwnd(self) -> HWND {
         HWND(windows_api::as_ptr!(self.hwnd))
@@ -369,15 +394,21 @@ impl Window {
     }
 
     pub fn center(&mut self, work_area: &Rect) -> Result<()> {
-        let half_width = work_area.right / 2;
-        let half_weight = work_area.bottom / 2;
+        let (aspect_ratio_width, aspect_ratio_height) = FLOATING_WINDOW_TOGGLE_ASPECT_RATIO
+            .lock()
+            .width_and_height();
+        let target_height = work_area.bottom / 2;
+        let target_width = (target_height * aspect_ratio_width) / aspect_ratio_height;
+
+        let x = work_area.left + ((work_area.right - target_width) / 2);
+        let y = work_area.top + ((work_area.bottom - target_height) / 2);
 
         self.set_position(
             &Rect {
-                left: work_area.left + ((work_area.right - half_width) / 2),
-                top: work_area.top + ((work_area.bottom - half_weight) / 2),
-                right: half_width,
-                bottom: half_weight,
+                left: x,
+                top: y,
+                right: target_width,
+                bottom: target_height,
             },
             true,
         )
@@ -872,7 +903,11 @@ fn window_is_eligible(
 
     let known_layered_hwnds = transparency_manager::known_hwnds();
 
-    allow_layered = if known_layered_hwnds.contains(&hwnd) {
+    allow_layered = if known_layered_hwnds.contains(&hwnd)
+        // we always want to process hide events for windows with transparency, even on other
+        // monitors, because we don't want to be left with ghost tiles
+        || matches!(event, Some(WindowManagerEvent::Hide(_, _)))
+    {
         debug.allow_layered_transparency = true;
         true
     } else {
@@ -924,12 +959,12 @@ fn window_is_eligible(
     }
 
     if (allow_wsl2_gui || allow_titlebar_removed || style.contains(WindowStyle::CAPTION) && ex_style.contains(ExtendedWindowStyle::WINDOWEDGE))
-                        && !ex_style.contains(ExtendedWindowStyle::DLGMODALFRAME)
-                        // Get a lot of dupe events coming through that make the redrawing go crazy
-                        // on FocusChange events if I don't filter out this one. But, if we are
-                        // allowing a specific layered window on the whitelist (like Steam), it should
-                        // pass this check
-                        && (allow_layered || !ex_style.contains(ExtendedWindowStyle::LAYERED))
+        && !ex_style.contains(ExtendedWindowStyle::DLGMODALFRAME)
+        // Get a lot of dupe events coming through that make the redrawing go crazy
+        // on FocusChange events if I don't filter out this one. But, if we are
+        // allowing a specific layered window on the whitelist (like Steam), it should
+        // pass this check
+        && (allow_layered || !ex_style.contains(ExtendedWindowStyle::LAYERED))
         || managed_override
     {
         return true;
