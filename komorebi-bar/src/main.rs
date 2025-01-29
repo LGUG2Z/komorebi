f5459fa8b3e2e1c7624dd46392a3dda3b4c8f4eb
alex-ds13 <145657253+alex-ds13@users.noreply.github.com>
2025-01-29 18:16:03 +0000

    fix(bar): handle monitor disconnect/reconnect
    This commit makes so a bar can be disabled when it's monitor is
    disconnected and re-enabled when said monitor reconnects.
    This commit also uses the new `monitor_usr_idx_map` to properly map the
    monitor index given by the users on config to the actual monitor index
    on the `WindowManager`, since in case some middle monitor gets
    disconnected the index of the monitors to the "right" of that one will
    be lowered by 1.
    This should allow for cases with monitor A, B and C, if monitor B
    disconnects, its bar will be disabled and monitor C will properly change
    its monitor index internally to 1 so it still gets the infos from
    monitor C (which now will have index 1 on the `WindowManager`).

--- komorebi-bar/src/main.rs
+++ komorebi-bar/src/main.rs
@@ -237,12 +237,16 @@ fn main() -> color_eyre::Result<()> {
         &SocketMessage::State,
     )?)?;
 
-    let (monitor_index, work_area_offset) = match &config.monitor {
+    let (usr_monitor_index, work_area_offset) = match &config.monitor {
         MonitorConfigOrIndex::MonitorConfig(monitor_config) => {
             (monitor_config.index, monitor_config.work_area_offset)
         }
         MonitorConfigOrIndex::Index(idx) => (*idx, None),
     };
+    let monitor_index = state
+        .monitor_usr_idx_map
+        .get(&usr_monitor_index)
+        .map_or(usr_monitor_index, |i| *i);
 
     MONITOR_RIGHT.store(
         state.monitors.elements()[monitor_index].size().right,
