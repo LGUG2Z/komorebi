d69dfeb715bd21e7ee3acdc6da0748fc2dd6cc8d
Csaba <georgelisabeth1231@gmail.com>
2025-02-23 17:45:27 +0100

    feat(bar): add opts to show all icons on workspace widget
    This commit adds 3 new display options on the komorebi workspace widget
    to show all icons.

--- komorebi-bar/src/config.rs
+++ komorebi-bar/src/config.rs
@@ -125,6 +125,15 @@ impl KomobarConfig {
             }
         }
     }
+
+    pub fn show_all_icons_on_komorebi_workspace(widgets: &[WidgetConfig]) -> bool {
+        widgets
+            .iter()
+            .any(|w| matches!(w, WidgetConfig::Komorebi(config) if config.workspaces.is_some_and(|w| w.enable && w.display.is_some_and(|s| matches!(s,
+            WorkspacesDisplayFormat::AllIcons
+            | WorkspacesDisplayFormat::AllIconsAndText
+            | WorkspacesDisplayFormat::AllIconsAndTextOnSelected)))))
+    }
 }
 
 #[derive(Clone, Debug, Serialize, Deserialize, JsonSchema)]
